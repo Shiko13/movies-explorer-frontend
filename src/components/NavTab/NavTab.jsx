4e04049100ae0e0262222994bb0b9aee8bbaaf43
Sergey <shikofreid@yandex.ru>
2023-08-01 00:02:48 +0400

    Added Promo

---
+++ src/components/NavTab/NavTab.jsx
@@ -0,0 +1,14 @@
+import "./NavTab.css";
+
+function NavTab() {
+    return (
+        <section className="navtab">
+            <a className="navtab__link" href="www.google.com">
+                Узнать больше
+            </a>
+        </section>
+    )
+}
+
+export default NavTab;
+
